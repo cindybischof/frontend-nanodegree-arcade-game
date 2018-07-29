e53ef109b7138b3a55e2134403e6c4ef647b2723
CindyBischof <cindy.bischof@gmail.com>
2018-07-29 15:29:32 -0500

    added char-pink-girl to the resources load

--- js/fg.js
+++ js/fg.js
@@ -91,10 +91,9 @@ var Engine = (function(global) {
      */
     function updateEntities(dt) {
         //allEnemies.forEach(function(enemy) {
-      //      enemy.update(dt);
-      //  });
-      //  player.update();
-      //COMMENTED OUT UNTIL I DECLARE THEM
+        //    enemy.update(dt);
+        //});
+        //player.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -150,12 +149,11 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        //allEnemies.forEach(function(enemy) {
-        //    enemy.render();
-        //});
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
 
-        //player.render();
-        //COMMENTED OUT TO USE LATER ON
+        player.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -175,8 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/char-pink-girl.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
