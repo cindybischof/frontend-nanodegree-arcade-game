525b7ae3fb1be6e96a199ca9fcb815e15b801235
CindyBischof <cindy.bischof@gmail.com>
2018-07-30 05:44:55 -0500

    we have 1 enemy bug

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 //class from which hero and enemies will both inherit overlapping characteristics
-class Entity {
+class Characters {
   constructor() {
     this.sprite = 'images/';
     this.x = 2;
     this.y = 5;
   }
 //offset because the images are all 101 x 83 pixels
+//offset allows player to move block by block
     render() {
       ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
   }
@@ -14,15 +15,38 @@ class Entity {
 //Hero class extends the Characters class from above
 //Uses super keyword to get this.sprite, this.x & this.y from the Characteristics class we're inheriting from,
 //refers to the constructor of the inherited class
-class Player extends Entity {
+class Player extends Characters {
   constructor() {
     super();
     this.sprite += 'char-pink-girl.png';
   }
 }
 
+//Enemy class extends the Characters class from above
+//Uses super keyword to get this.sprite, this.x & this.y from the Characteristics class we're inheriting from,
+//passes in parameters to get different starting points on the x & y axes than the player sprite
+class Enemy extends Characters {
+  constructor(x,y) {
+    super();
+    this.sprite += 'enemy-bug.png';
+    this.x = x;
+    this.y = y;
+  }
+}
+
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 //creats an instance of the Hero
 const player = new Player();
+
+
+
+//creates instances of the Enemy bug
+const bug1 = new Enemy(1,1);
+const allEnemies = [];
+allEnemies.push(bug1);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
