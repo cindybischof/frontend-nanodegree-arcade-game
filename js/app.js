8253b600c1c3128cad63005a5cf76e927cf6a478
CindyBischof <cindy.bischof@gmail.com>
2018-07-31 05:14:46 -0500

    added a fourth bug

--- js/app.js
+++ js/app.js
@@ -91,6 +91,9 @@ allEnemies.push(bug2);
 const bug3 = new Enemy(0,3,1);
 allEnemies.push(bug3);
 
+const bug4 = new Enemy(1,2,.5);
+allEnemies.push(bug4);
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
